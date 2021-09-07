b7739f705ad8be78ba5520fd2d5be0863957f3c4
Jason Jun <uxd.jun@gmail.com>
2021-09-07 21:14:16 +1000

    Create basic scaffold

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,8 +1,28 @@
-import Head from 'next/head'
-import Image from 'next/image'
-
-export default function Home() {
+const Home = () => {
   return (
-    <div>Home</div>
+    <>
+      <aside>
+        <h1>Hacker News</h1>
+        <nav>
+          <ul>
+            <li>Top stories</li>
+            <li>New stories</li>
+            <li>Ask HN</li>
+            <li>Show HN</li>
+            <li>Jobs</li>
+          </ul>
+        </nav>
+      </aside>
+      <main>
+        <input type="text" placeholder="Search..."/>
+        <ul>
+          <li>Story item</li>
+        </ul>
+      </main>
+      <aside></aside>
+
+    </>
   )
 }
+
+export default Home
