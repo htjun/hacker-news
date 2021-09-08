e75fde7a638bbfc03900a987de28573bbf7eb280
Jason Jun <uxd.jun@gmail.com>
2021-09-08 20:51:40 +1000

    Set destyle and global style

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,4 +1,14 @@
 import type { AppProps } from 'next/app'
+require('destyle.css')
+import { createGlobalStyle } from "styled-components"
+
+export const GlobalStyle = createGlobalStyle`
+  * {
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;
+
+  }
+`
 
 function MyApp({ Component, pageProps }: AppProps) {
   return <Component {...pageProps} />
