0afabf6a09ac9c6f261505b1282befc3998b0847
Jason Jun <uxd.jun@gmail.com>
2021-09-09 13:26:21 +1000

    Add typography styles

--- src/styles/style.tsx
+++ src/styles/style.tsx
@@ -18,3 +18,37 @@ export const fontSet = {
   sans: `'Inter', sans-serif`,
   serif: `serif`,
 }
+
+export const fontSize = {
+  xs: '0.75rem',
+  sm: '0.875rem',
+  base: '1rem',
+  lg: '1.125rem',
+  xl: '1.25rem',
+  xl2: '1.5rem',
+  xl3: '2rem',
+  xl4: '2.5rem',
+  xl5: '3rem',
+  xl6: '3.25rem',
+}
+
+export const fontWeight = {
+  regular: 400,
+  medium: 500,
+  semibold: 600,
+  bold: 700,
+  black: 900,
+}
+
+export const textLineHeight = {
+  tight: 1.25,
+  normal: 1.5,
+  loose: 1.65,
+}
+
+export const textLetterSpacing = {
+  tighter: '-0.04em',
+  tight: '-0.025em',
+  normal: 0,
+  loose: '0.025em',
+}
