1a4b9afbd4805aaa9b37867ac28bc90a80e5e533
Jason Jun <uxd.jun@gmail.com>
2021-09-10 09:31:36 +1000

    Update shortenUrl helper
    change regex to support more browsers

--- src/helpers/shortenUrl.tsx
+++ src/helpers/shortenUrl.tsx
@@ -1,7 +1,13 @@
-const urlRegex = /(?<=\/\/).+?(?=\/)/g
+const urlRegex = /\/\/[www.]*(.*?)\//
 
-const shortenUrl = (url: string) => {
-  return url && url.match(urlRegex)
+const shortenUrl = (url: any) => {
+  let shortenedUrl = []
+
+  if (url) {
+    shortenedUrl = url.match(urlRegex)
+  }
+
+  return shortenedUrl && shortenedUrl[1]
 }
 
 export default shortenUrl
