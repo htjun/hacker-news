e40d09a89cd5968caafc6c591b410eaf2ae3a0ea
Jason Jun <uxd.jun@gmail.com>
2021-09-09 13:25:21 +1000

    Add HSL color builder

--- src/styles/style.tsx
+++ src/styles/style.tsx
@@ -1,3 +1,19 @@
+// Color
+export const hsl = (color: string, lightness: number) => {
+  let hs: string
+
+  switch (color) {
+    case "neutral":
+      hs = "216, 8%"
+      break;
+    default:
+      hs = "216, 8%"
+      break;
+  }
+  return `hsl(${hs}, ${lightness}%)`
+}
+
+// Typography
 export const fontSet = {
   sans: `'Inter', sans-serif`,
   serif: `serif`,
