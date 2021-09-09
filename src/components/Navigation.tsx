f653ba05063c752fc743aad675f85a8a4e0f74f2
Jason Jun <uxd.jun@gmail.com>
2021-09-09 15:57:16 +1000

    Update Navigation Style with Icons

--- src/components/Navigation.tsx
+++ src/components/Navigation.tsx
@@ -1,6 +1,11 @@
 import Link from 'next/link'
 import { useRouter } from 'next/router'
-import { NavWrapper } from 'src/styles/Navigation.style'
+import { NavWrapper, NavItem } from 'src/styles/Navigation.style'
+import IconGraphUp from 'src/assets/icons/IconGraphUp.svg'
+import IconHandUp from 'src/assets/icons/IconHandUp.svg'
+import IconIdCard from 'src/assets/icons/IconIdCard.svg'
+import IconMegaphone from 'src/assets/icons/IconMegaphone.svg'
+import IconSparkle from 'src/assets/icons/IconSparkle.svg'
 
 const menu = [
   { title: 'Top stories', path: '/' },
@@ -9,6 +14,28 @@ const menu = [
   { title: 'Show HN', path: '/show' },
   { title: 'Jobs', path: '/jobs' },
 ]
+
+const getMenuIcon = (path: string) => {
+  switch (path) {
+    case '/':
+      return <IconGraphUp />
+      break;
+    case '/new':
+      return <IconSparkle />
+      break;
+    case '/ask':
+      return <IconHandUp />
+      break;
+    case '/show':
+      return <IconMegaphone />
+      break;
+    case '/jobs':
+      return <IconIdCard />
+      break;
+    default:
+      break;
+  }
+}
 
 const Navigation = () => {
   const router = useRouter()
@@ -20,13 +47,16 @@ const Navigation = () => {
         <ul>
           {menu.map((item, index) => {
             return (
-              <li key={index} className={
+              <NavItem key={index} className={
                 router.pathname === item.path ? `active` : ``
               }>
                 <Link href={item.path}>
-                  {item.title}
+                  <a>
+                    {getMenuIcon(item.path)}
+                    <span>{item.title}</span>
+                  </a>
                 </Link>
-              </li>
+              </NavItem>
         )
       })}
         </ul>
--- src/styles/Navigation.style.tsx
+++ src/styles/Navigation.style.tsx
@@ -1,5 +1,40 @@
 import styled from 'styled-components'
+import * as style from 'src/styles/style'
 
 export const NavWrapper = styled.aside`
   display: block;
+  width: 180px;
+`
+
+export const NavItem = styled.li`
+  margin-bottom: 4px;
+
+  a {
+    display: flex;
+    align-items: center;
+    padding: 6px;
+    border-radius: 6px;
+    font-size: ${style.fontSize.sm};
+    font-weight: ${style.fontWeight.medium};
+    line-height: ${style.textLineHeight.normal};
+    color: ${style.hsl("neutral", 64)};
+
+    svg {
+      width: 16px;
+      height: 16px;
+      margin-right: 8px;
+      fill: currentColor;
+      opacity: 0.8;
+    }
+
+    &:hover {
+      background-color: ${style.hsl("neutral", 98)};
+    }
+  }
+
+  &.active {
+    a {
+      color: ${style.hsl("neutral", 4)};
+    }
+  }
 `
