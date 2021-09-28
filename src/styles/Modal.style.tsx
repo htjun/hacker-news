69f94b45c6552609642147ab7810ecd6488b9fb9
Jason Jun <uxd.jun@gmail.com>
2021-09-28 10:53:13 +1000

    Update Modal.style.tsx

--- src/styles/Modal.style.tsx
+++ src/styles/Modal.style.tsx
@@ -1,6 +1,7 @@
 import styled from "styled-components"
+import * as style from "src/styles/style"
 
-export const Overay = styled.div`
+export const Overlay = styled.div`
   width: 100vw;
   height: 100vh;
   background-color: rgba(0, 0, 0, 0.8);
@@ -16,4 +17,13 @@ export const ModalContainer = styled.div`
   padding: 32px;
   border-radius: 6px;
   background-color: #fff;
+  color: ${({ theme }) => theme.text.neutral.l1};
+
+  h1 {
+    font-family: ${style.fontSet.heading};
+    font-size: ${style.fontSize.lg};
+    font-weight: ${style.fontWeight.bold};
+    line-height: ${style.textLineHeight.tight};
+    margin-bottom: 12px;
+  }
 `
