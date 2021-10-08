743ffb3ebee4dc143f7274972a677d2382f51e23
Jason Jun <uxd.jun@gmail.com>
2021-10-09 10:32:23 +1100

    Add Back to top button

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -1,13 +1,14 @@
-import { LayoutOuter, LayoutInner } from 'src/styles/Layout.style'
+import { LayoutOuter, LayoutInner } from "src/styles/Layout.style"
+import BackToTopBtn from "src/components/BackToTopButton"
 
 const Layout = (props: any) => {
-
   return (
-    <LayoutOuter>
-      <LayoutInner>
-        {props.children}
-      </LayoutInner>
-    </LayoutOuter>
+    <>
+      <LayoutOuter>
+        <LayoutInner>{props.children}</LayoutInner>
+      </LayoutOuter>
+      <BackToTopBtn />
+    </>
   )
 }
 
