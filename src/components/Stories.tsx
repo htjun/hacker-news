064980db9337916e7bb14344df7657905d1b7c03
Jason Jun <uxd.jun@gmail.com>
2021-10-14 20:58:23 +1100

    Add Load More button

--- src/components/Stories.tsx
+++ src/components/Stories.tsx
@@ -1,8 +1,15 @@
-import { StoriesWrapper } from "src/styles/Stories.style"
+import { useState } from "react"
+import { StoriesWrapper, LoadMorebutton } from "src/styles/Stories.style"
 import Story from "src/components/Story"
 
 const Stories = (props: any) => {
   const { data, searchTerm } = props
+  const [itemCount, setItemCount] = useState(50)
+
+  const handleLoadMoreClick = (e: any) => {
+    e.preventDefault()
+    setItemCount(itemCount + 50)
+  }
 
   return (
     <StoriesWrapper>
@@ -15,9 +22,15 @@ const Stories = (props: any) => {
               )
             }
           })
+          .slice(0, itemCount)
           .map((item: any, index: number) => {
             return <Story key={item ? item.id : index} data={item && item} />
           })}
+      {itemCount < data.length && (
+        <LoadMorebutton onClick={handleLoadMoreClick}>
+          Load more stories..
+        </LoadMorebutton>
+      )}
     </StoriesWrapper>
   )
 }
